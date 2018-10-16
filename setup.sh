195a74defd9226d94cee3889a6e10726fa549c2c
Scumtron <scumtron@mail.ru>
2018-10-16 23:23:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -509,7 +509,7 @@ EOF
 	
 	. /etc/profile
 	
-	cd /tmp && wget "http://cdn.ispsystem.com/install.sh" && sh install.sh ISPmanager --silent
+	cd /tmp && wget "http://cdn.ispsystem.com/install.sh" && sh install.sh ISPmanager --silent --release stable
 else
 	if [ "#${fpm}" == "#true" ]; then
 		wget http://mirrors.linuxeye.com/oneinstack-full.tar.gz && tar xzf oneinstack-full.tar.gz && ./oneinstack/install.sh --nginx_option 1 --php_option 7 --phpcache_option 1 --pureftpd 
