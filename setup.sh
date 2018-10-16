c5e7f0c1472d9aaad1253e2d5d067628607f4f2f
Scumtron <scumtron@mail.ru>
2018-10-16 14:49:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #
-# chmod +x setup.sh && ./setup.sh
+# wget https://github.com/Scumtron/Firewall/raw/master/setup.sh && chmod +x setup.sh && ./setup.sh
 #
 
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
-yum update 
-yum -y install epel-release
+yum update -y
+yum install -y epel-release
 rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
 rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
-yum update
-yum install -y mc nano htop iftop wget curl
+yum update -y
+yum install -y nano mc htop iftop curl memcached ffmpeg imagemagick
+echo "export EDITOR=nano" >> /root/.bashrc
 echo "Done"
